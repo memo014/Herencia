eae764d377f9d63081ed0527cbb9af2ecd17bcc4
Alumno <alumno@localhost.localdomain>
2014-02-20 08:10:14 -0600

    Herencia

---
+++ Claseejemplo/src/Claseejemplo.java
@@ -0,0 +1,17 @@
+
+class A
+{
+public char x;
+
+}
+
+
+public class Claseejemplo {
+
+	public static void main(String[] args) {
+		A a = new A();
+		a.x='b';
+
+	}
+
+}
---
+++ Herencia/src/com/me/mygdxgame/Claseejemplo.java
@@ -0,0 +1,6 @@
+package com.me.mygdxgame;
+
+public class Claseejemplo {
+	
+
+}
